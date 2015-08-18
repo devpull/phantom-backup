4ca5d2d4d4fb09bbd9a24209442c0cf3b4d24b4c
truetamtam <truetamtam@yandex.ru>
2015-08-18 22:33:44 +0300

    pahn_bck go

---
+++ README.md
@@ -0,0 +1,11 @@
+# Backup from ispmanager control panel
+---
+
+## Purpose of this
+Backuping from shared hosting by hands was not an option.  
+CURL is not for javascript execution. For js execution using phantomjs and casperjs.
+
+## Requirements (tested with phantomjs v1.9.8 & casperjs v1.1.0-beta3)
+* phantomjs
+* casperjs
+* curl
---
+++ casp.js
@@ -0,0 +1,65 @@
+// config
+var fs = require('fs'), configFile = fs.read('./local/config.json'), config = JSON.parse(configFile);
+// casper create
+var casper = require("casper").create({
+    verbose: true,
+    logLevel: 'debug',
+    pageSettings: {
+        loadImages: false,
+        loadPlugins: false,
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
+    }
+});
+
+// log
+function log(msg) {
+    var date = new Date();
+    var day = ('0' + date.getDate()).slice(-2), month = ('0' + (date.getMonth()+1)).slice(-2), year = date.getFullYear();
+    var logFileName = day + "" + month + "" + year + '.log';
+    var filePath = fs.workingDirectory + '/logs/' + logFileName;
+    // creating log file
+    if(!fs.exists(filePath)) {
+        fs.write(filePath, msg + '\n', 'w');
+    } else {
+        fs.write(filePath, msg + '\n', 'a');
+    }
+}
+
+// print out all the messages in the headless browser context
+casper.on('remote.message', function(msg) {
+    log(msg);
+    this.echo('remote message caught: ' + msg);
+});
+
+// print out all the messages in the headless browser context
+casper.on("page.error", function(msg, trace) {
+    this.echo("Page Error: " + msg, "ERROR");
+});
+
+// print out all the messages in the headless browser context
+casper.on("resource.error", function(resourceError){
+    console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
+    console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
+});
+
+// url is set in config
+casper.start(config.url, function() {
+    this.echo('Starting.');
+});
+
+// wait for page to load
+casper.waitForResource(config.url, function() {
+    this.echo('trying fill form');
+
+    // fill selectors not working with phantomjs v1.9.8 & casperjs v1.1.0-beta3
+    this.fillXPath('form', {
+        '//input[@name="username"]': config.username,
+        '//input[@name="password"]': config.password
+    }, true);
+
+    this.capture('screen/fill.png');
+}).then(function() {
+    this.wait(5000, function () {
+        this.capture('screen/done.png');
+    });
+}).run();
